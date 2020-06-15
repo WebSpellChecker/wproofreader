9f8ac920387160d23f08948cd2896a16d813d0b8
wsc-team-city <github@webspellchecker.net>
2020-06-15 08:09:18 +0000

    files for release 5.6.0.1

--- wscbundle/local/zh-cn/local.js
+++ wscbundle/local/zh-cn/local.js
@@ -1,5 +1,5 @@
 /**
- * scayt v2.4.4089
+ * scayt v2.4.4191
  * Copyright (c) 2000-2020 WebSpellChecker LLC All rights reserved.
  */
 
@@ -28,5 +28,13 @@
  * Released under the MIT license
  * Author: Miller Medeiros
  * Version: 0.8.1 - Build: 266 (2012/07/31 03:33 PM)
- */ 
+ */
+
+/*!
+* flag-icon-css
+* https://github.com/lipis/flag-icon-css
+*
+* Copyright 2013 Panayiotis Lipiridis
+* Licensed under the MIT license.
+*/ 
 !function(a,b){a&&(a.prototype.Localization.lang[b]={btn_about:"关于 WebSpellChecker",btn_addWord:"加字",btn_createDic:"创建",btn_deleteDic:"删除",btn_disable:"禁用",btn_enable:"启用",btn_ignore:"忽视",btn_ignoreAll:"忽略所有",btn_langs:"語言",btn_moreSuggestions:"更多建议",btn_noSuggestions:"没有建议",btn_options:"选件",btn_renameDic:"改名",btn_restoreDic:"恢复",btn_toggle:"切换",btn_dictionaries:"辞典",btn_undo:"撤消",btn_redo:"重做",btn_cut:"切",btn_copy:"复制",btn_paste:"糊",btn_cancel:"取消",btn_help:"救命",btn_info:"信息",btn_apply:"应用",btn_settings:"设置",btn_close:"关闭",btn_back:"背部",btn_edit:"编辑",btn_connectDic:"连接",btn_disconnectDic:"断开",btn_moreLanguages:"详细了解如何添加更多语言",btn_learnDictionary:"进一步了解如何使用字典",btn_add:"加",btn_dictionaryPreferences:"字典首选项",btn_goToDic:"转到字典",btn_deleteWord:"删除单词",btn_userManual:"有关更多信息，请参见WebSpellChecker用户手册",btn_showSection:"显示部分",btn_hideSection:"隐藏部分",btn_showMore:"展示更多",btn_showLess:"显示较少",btn_proofreadDialog:"对话框中的校对",btn_finishChecking:"完成检查",btn_maximize:"最大化",btn_restore:"恢复",btn_leave:"是的，退出",btn_stay:"不，留下来",btn_prevProblem:"上一个问题",btn_nextProblem:"下一个问题",placeholder_fieldNameDic:"输入字典名称",placeholder_fieldAddWord:"输入一个新词",label_ignoreAllCapsWords:"忽略大写单词",label_fieldNameDic:"字典名称",label_ignoreDomainNames:"忽略域名",label_ignoreWordsWithMixedCases:"忽略大小写混合的单词",label_ignoreWordsWithNumbers:"忽略带数字的单词",message_info_emptyDic:"字典名称不能为空。",message_info_opera:"Opera不支持",message_info_cut:"剪切操作仅在使用键盘快捷键的浏览器中可用。 使用Ctrl / Cmd + X",message_info_copy:"复制操作仅在使用键盘快捷键的浏览器中可用。 使用Ctrl / Cmd + C",message_info_paste:"使用键盘（Ctrl / Cmd + V）在此处插入文本，然后单击粘贴.",about:"关于",tab_dictionaries:"辞典",tab_languages:"語言",tab_options:"选件",text_title:"WProofreader",text_productActions:"WProofreader 行动",text_correctionBox:"修正框",text_copyrights:"<p class='wsc-copyright__text'>&copy; 2020 WebSpellChecker LLC. All Rights Reserved.</p>",about_throw_copy:"<p class='wsc-copyright__text'>&copy; 2020 WebSpellChecker LLC. All Rights Reserved.</p>",text_descriptionDicForFree:"使用WebSpellChecker产品的免费版本，添加到个人词典中的所有单词都存储在浏览器本地存储中。为了保存个人字典并从各种浏览器或设备访问它，该字典应存储在WebSpellChecker服务器上。此选项仅在付费版本中可用。",text_descriptionDicForPaid:"详细了解如何使用字典工作",text_descriptionDic:"默认情况下，添加到个人词典中的所有单词都存储在浏览器本地存储中。要在各种浏览器和设备上访问和使用您的个人词典，请单击创建，您的词典将保存在服务器上。",text_spellingSectionHeader:"拼写建议",text_grammarSectionHeader:"语法建议",text_dictionaryWords:"字典单词",text_suggestionsContainer:"建议容器",text_problem:"问题",text_changeTo:"改成",text_problemsCount:"问题数",version:"版: ",message_success_createDic:"您已成功创建'%s'词典。",message_error_createDic:"无法创建字典'%s'。 可能是由于名称重复或无效。 请查看具有该名称的字典是否已经存在，如果没有，请提供新字典的名称。",message_success_removeDic:"您已成功删除'%s'词典。",message_error_removeDic:"字典'%s'无法删除。 我们尚未找到具有该名称的字典。 请检查它是否仍然存在。",message_success_restoreDic:"您已成功连接'%s'词典。",message_error_restoreDic:"字典'%s'无法连接。 我们尚未找到具有该名称的字典。 请检查它是否仍然存在。",message_success_disconnectDic:"您已成功断开'%s'词典的连接。",message_error_disconnectDic:"无法断开字典'%s'的连接。",message_success_renameDic:"您已成功重命名了'%s'词典。",message_error_renameDic:"字典'%s'无法重命名。",message_success_deleteWordDic:"您已成功删除'%s'一词。",message_error_deleteWordDic:"不能删除单词'%s'。 我们在您的字典中找不到这样的单词。 请检查它是否仍然存在。",message_success_addWordDic:"您已成功添加'%s''一词",message_error_addWordDic:"不能添加单词'%s'。",message_success_wordAlreadyAddedDic:"单词'%s'已经在字典中。",message_error_wordWithBannedSymbolsDic:"不能添加单词'%s'。 请确保它仅包含字母和数字。",message_confirm_closeWithoutSave:"你确定要离开？ 如果不单击“完成检查”，所有更改将丢失。",oldck_btn_userManual:"有关更多信息，请参见SCAYT用户手册。",oldck_tab_about:"关于 SCAYT",oldck_btn_toggle:"切换 SCAYT",oldck_text_descriptionDicForFree:"使用SCAYT产品的免费版本，添加到个人词典中的所有单词都存储在浏览器本地存储中。 为了保存个人字典并从各种浏览器或设备访问它，该字典应存储在WebSpellChecker服务器上。 该选项在 <a href='https://www.webspellchecker.net/signup/hosted-signup.html#scayt-paid' target='_blank' style='text-decoration: underline; color: blue;'>付费版本</a> 只要。",oldck_text_descriptionDicForPaid:"<a href='http://scayt.com/user_manual/scayt_plugin_for_ckeditor4_user_manual.pdf' target='_blank' style='text-decoration: underline; color: blue;'>详细了解如何使用字典工作</a>"})}(window.WEBSPELLCHECKER.CORE,"zh-cn");
